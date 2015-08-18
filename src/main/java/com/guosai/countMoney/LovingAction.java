ba0932f86e29b47dc62d38f46565851870b5a915
qiyang <15839665935@163.com>
2015-08-18 18:19:34 +0800

    first commit

--- src/main/java/com/guosai/countMoney/LovingAction.java
+++ src/main/java/com/guosai/countMoney/LovingAction.java
@@ -1,21 +1,25 @@
 package com.guosai.countMoney;
 
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
  * Created by qiyang on 15-8-18.
  */
 public class LovingAction {
-    public void action(String file1,String file2){
-        POIFSFileSystem fs1;
-        POIFSFileSystem fs2;
+    public void action(String file1, String file2) {
+        XSSFWorkbook xssfWorkbook_mon;
+        XSSFWorkbook xssfWorkbook_all;
+        XSSFSheet xssfSheet_mon;
+        XSSFSheet xssfSheet_all;
+
         try {
-            fs1 = new POIFSFileSystem(new FileInputStream(file1));
-            fs2 = new POIFSFileSystem(new FileInputStream(file2));
+            xssfWorkbook_mon = new XSSFWorkbook(file1);
+            xssfWorkbook_all = new XSSFWorkbook(file2);
+            xssfSheet_mon = xssfWorkbook_mon.getSheet("");
+            xssfSheet_all = xssfWorkbook_all.getSheet("");
         } catch (IOException e) {
             e.printStackTrace();
         }
